099b01f27e73647fe597cd15b40cd2c7f675dcc0
Simply Done <matlurulikhitha@gmail.com>
2025-05-02 15:56:33 +0530

    mmt hotel code

---
+++ src/main/java/com/tp/mmthotel/MmthotelApplication.java
@@ -0,0 +1,13 @@
+package com.tp.mmthotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmthotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmthotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/mmthotel/MmthotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.mmthotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmthotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
